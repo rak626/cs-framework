f5de43842e2db08cfb89fa183193fc2c67f1dea3
Rakesh Ghosh <rakeshacot@gmail.com>
2025-01-01 03:41:37 +0530

    Update ShowTime.java

--- cs-common/src/main/java/com/crowdseats/framework/common/schema/event/ShowTime.java
+++ cs-common/src/main/java/com/crowdseats/framework/common/schema/event/ShowTime.java
@@ -3,11 +3,13 @@ package com.crowdseats.framework.common.schema.event;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ShowTime {
     private String showTimeId;
-    private String date;
+    private Date showDate;
     private String startTime;
     private String endTime;
 }
